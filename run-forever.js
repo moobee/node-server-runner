e7f974aa6fe7b3c535d1eafa96cb460727c73b70
v.kayser <v.kayser@moobee.fr>
2015-06-12 11:42:59 +0200

    Crée le script qui lance en permanance le serveur via forever-monitor : envoi un mail lorsqu'il a été relancé à l'administrateur

---
+++ run-forever.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var config = require('./config.js');
+
+var forever = require('forever-monitor');
+
+var nodemailer = require('nodemailer');
+var sendmailTransport = require('nodemailer-sendmail-transport');
+
+var argv = require('minimist')(process.argv.slice(2));
+
+
+// si la ligne de commande a été lancée avec le nom du fichier du server avec le nom du fichier de log
+if (process.argv[2] && argv.logFile) {
+	var child = new (forever.Monitor)(process.argv[2], {
+		silent: true,
+		args: [],
+		errFile: argv.logFile, // logs
+	});
+
+	console.log('Start server ...');
+
+	// à chaque restart
+	child.on('restart', function (err) {
+		console.log('error : consulter le fichier log ' + argv.logFile);
+
+		var transporter = nodemailer.createTransport(sendmailTransport({
+		    path: 'sendmail',
+		    args: [ '-t', '-i' ],
+		}));
+
+		// envoi du mail a l'admin
+		transporter.sendMail({
+		    from: config.sender,
+		    to: config.admin,
+		    subject: 'Error node server',
+		    text: 'Une erreur est survenue sur le serveur, consultez les logs dans : ' + argv.logFile,
+		    replyTo: config.noreplyAddress,
+		}, function (err) {
+			if (err) {
+				console.log(err);
+			}
+		});
+	});
+
+
+	child.start();
+}
+else {
+	console.log('Arguments manquants\nUtilisez : node run-forever.js path/to/mon-fichier.js --logFile  path/to/fichierLog.log');
+}
+
+

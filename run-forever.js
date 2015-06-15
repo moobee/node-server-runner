816950a0e664cac626cee6ec67246b2576d53676
Dorian MARCHAL <d.marchal@moobee.fr>
2015-06-15 09:52:07 +0200

    Ajoute une durée minimum à respecter entre deux crashs

--- run-forever.js
+++ run-forever.js
@@ -9,7 +9,8 @@ var sendmailTransport = require('nodemailer-sendmail-transport');
 
 var argv = require('minimist')(process.argv.slice(2));
 
-var restartCount = 0;
+var loopRestartCount = 0;
+var lastCrashTime;
 
 var transporter = nodemailer.createTransport(sendmailTransport({
     path: 'sendmail',
@@ -37,6 +38,7 @@ var sendMail = function (subject, text) {
 
 // si la ligne de commande a été lancée avec le nom du fichier du server avec le nom du fichier de log
 if (process.argv[2] && argv.logFile) {
+
     var child = new (forever.Monitor)(process.argv[2], {
         silent: true,
         args: [],
@@ -49,12 +51,26 @@ if (process.argv[2] && argv.logFile) {
     // à chaque restart
     child.on('restart', function () {
 
-        restartCount++;
+        var currentTime = new Date().getTime();
 
-        if (restartCount >= config.maxRestartCount) {
+        var timeFromLastCrash = lastCrashTime ? currentTime - lastCrashTime : 0;
+
+        console.log('Précédent crash il y a : ' + timeFromLastCrash + 'ms');
+
+        if (!lastCrashTime || timeFromLastCrash <= config.minTimeBetweenCrashes) {
+            loopRestartCount++;
+        }
+        else {
+            loopRestartCount = 1;
+        }
+        console.log('Le serveur a crashé ' + loopRestartCount + ' fois de suite.');
+
+        if (loopRestartCount >= config.maxRestartCount) {
             sendMail('Le serveur a crashé', 'Le serveur crashait en boucle et a été définitivement arrêté après ' + config.maxRestartCount + ' crashs');
             process.exit();
         }
+
+        lastCrashTime = currentTime;
 
         sendMail('Le serveur a redémarré', 'Une erreur est survenue sur le serveur ' + process.argv[2] + ', consultez les logs dans : ' + argv.logFile);
 
